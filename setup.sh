8b57feed2f0ac10994f741e614cc70654b935597
Thomas Hipp <thomashipp@gmail.com>
2012-08-30 21:02:49 +0200

    Modified setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo ln -fsv "${PWD}/profile" /etc/profile
-sudo ln -fsv "${PWD}/bash.bashrc" /etc/bash.bashrc
+#sudo ln -fsv "${PWD}/profile" /etc/profile
+#sudo ln -fsv "${PWD}/bash.bashrc" /etc/bash.bashrc
 
 sudo ln -fsv "${PWD}/vimrc" /etc/vimrc
-sudo ln -fsv "${PWD}/wombat2562.vim" /usr/share/vim/vim73/colors/wombat256.vim
+sudo ln -fsv "${PWD}/wombat2562.vim" /usr/share/vim/vim73/colors/wombat2562.vim
 
 ln -fsv "${PWD}/xinitc" ~/.xinitc
 ln -fsv "${PWD}/Xresources" ~/.Xresources
@@ -12,4 +12,4 @@ ln -fsv "${PWD}/Xresources" ~/.Xresources
 ln -fsv "${PWD}/i3config" ~/.i3/config
 ln -fsv "${PWD}/i3status.conf" ~/.i3status.conf
 
-ln -fsv "${PWD}/zshrc" ~/.zshrc
+#ln -fsv "${PWD}/zshrc" ~/.zshrc
